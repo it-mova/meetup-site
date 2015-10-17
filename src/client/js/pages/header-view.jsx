042d020c50fbcee1634f7d62a7dec87e04ebef7a
vaukalak <lahmataja.pa4vara@gmia>
2015-10-17 22:31:29 +0300

    added timer to header section (need to fix layout)

--- src/client/js/pages/header-view.jsx
+++ src/client/js/pages/header-view.jsx
@@ -1,11 +1,60 @@
+var TimerView = React.createClass({
+    getInitialState: function(){
+        return { timeLeft: this.getTimeLeft() };
+    },
+    componentDidMount() {
+        this.timer = setInterval(this.tick, 50);
+    },
+    componentWillMount() {
+        clearInterval(this.tick);
+    },
+    tick() {
+        this.setState({timeLeft: this.getTimeLeft()});
+    },
+    getTimeLeft() {
+        return new Date('2015-11-14T11:00:00').getTime() - new Date().getTime();
+    },
+    render() {
+        var format = time => `${time < 10 ? '0' : ''}${time}`;
+        var timeLeft = Math.round(this.state.timeLeft / 1000);
+        var seconds = timeLeft % 60;
+        var minutes = (timeLeft - seconds) / 60 % 60;
+        var hours = (timeLeft - seconds - minutes * 60) / (60 * 60) % 24;
+        var days = Math.floor(timeLeft / (60 * 60 * 24));
+        return (
+            <div className="intro-heading">
+                <ul>
+                    <li>
+                        <div>{days}</div>
+                        <p>Дзён</p>
+                    </li>
+                    <li>
+                        <div>{format(hours)}</div>
+                        <p>Гадзін</p>
+                    </li>
+                    <li>
+                        <div>{format(minutes)}</div>
+                        <p>Хвілін</p>
+                    </li>
+                    <li>
+                        <div>{format(seconds)}</div>
+                        <p>Секунд</p>
+                    </li>
+                </ul>
+                <br/>
+            </div>
+        )
+    }
+});
+
 var HeaderView = React.createClass({
     render: () =>
         <header>
             <div className="container">
                 <div className="intro-text">
-                    <div className="intro-lead-in">Welcome To Our Studio!</div>
-                    <div className="intro-heading">It's Nice To Meet You</div>
-                    <a href="#services" className="page-scroll btn btn-xl">Tell Me More</a>
+                    <div className="intro-lead-in">Да сустрэчы засталося</div>
+                    <TimerView/>
+                    <a href="#howtofind" className="page-scroll btn btn-xl">Далей</a>
                 </div>
             </div>
         </header>
