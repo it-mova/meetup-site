225379706b3c097af00794a0c69a99a415d1f6b5
Shadow of Wolf <the.shadow.of.wolf@gmail.com>
2015-10-30 14:39:17 +0300

    added more texts

--- src/client/js/pages/application-view.jsx
+++ src/client/js/pages/application-view.jsx
@@ -28,7 +28,7 @@ var topics = [
     image: "img/topics/slack.png",
     ref: "slackModal",
     shortDescription: "Камунікацыя новага пакалення",
-    longDescription: '<div><p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p> <p> <strong>Want these icons in this portfolio item sample?</strong>You can download 60 of them for free, courtesy of <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">RoundIcons.com</a>, or you can purchase the 1500 icon set <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">here</a>.</p></div>'
+    longDescription: '<div><p><b>Slack</b> my pitch up! Альбо як звесці блізкасць зносін каманды да непрыстойнасці</p></div>'
   },
   {
     speaker: "Міхась Бутылін",
@@ -36,7 +36,7 @@ var topics = [
     image: "img/topics/es6.png",
     ref: "ecmaScriptModal",
     shortDescription: "Новыя магчымасці JavaScript",
-    longDescription: '<div><p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p> <p> <strong>Want these icons in this portfolio item sample?</strong>You can download 60 of them for free, courtesy of <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">RoundIcons.com</a>, or you can purchase the 1500 icon set <a href="https://getdpd.com/cart/hoplink/18076?referrer=bvbo4kax5k8ogc">here</a>.</p></div>'
+    longDescription: '<div>Пасля таго, як <b>node.js</b> пачала падтрымліваць асноўныя магчымасці <b>ES6</b>, а кампілятары кшталту <b>Babel</b> падтрымліваюць больш за 70% функцыаналу - актуальнасць <b>EcmaScript 2015</b> высокая як ніколі!<br/>Вы пачуеце аб тым якія з гэтых магчымасцяў вы можаце выкарыстаць ужо сёння і пабачыце шмат тлумачальных прыкладаў па тэме!</p></div>'
   }
 ]
 
